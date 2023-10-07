e0773aeb270573a46d60b61032550dba04f0ec79
Niket Patadiya <121963451+Niket-ketanbhai-Patadiya@users.noreply.github.com>
2023-10-07 13:20:23 +0000

    strings added

--- 01_Basics/05_Strings.js
+++ 01_Basics/05_Strings.js
@@ -5,5 +5,29 @@ const repoCount=10
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName=new String('NikNp');
+const gameName=new String('hitesh-hc-com');
 console.log(gameName);
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('k'));
+
+const newString= gameName.substring(0,4)
+console.log(newString);
+
+const anotherString=gameName.slice(-8,3)
+console.log(anotherString);
+
+const newStringOne="    niket    "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+const url="https://nikes.com/niketh%20patadiya"
+
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('sundar'));
+
+console.log(gameName.split('-'));
