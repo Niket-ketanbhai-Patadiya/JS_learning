313d1e89228bb92ac72e0a16532dd8f9d36c0100
Niket Patadiya <121963451+Niket-ketanbhai-Patadiya@users.noreply.github.com>
2023-10-07 08:27:43 +0000

    Strings

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,9 @@
+const name="nik"
+const repoCount=10
+
+// console.log(name+ repoCount + "Value"); do not write like these make code redable
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName=new String('NikNp');
+console.log(gameName);
