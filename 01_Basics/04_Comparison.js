2a706847ee273cea1b1280093f34039fd8a38a6b
Niket Patadiya <121963451+Niket-ketanbhai-Patadiya@users.noreply.github.com>
2023-10-07 07:44:50 +0000

    comparisons

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,19 @@
+// console.log("nik")
+// console.log(2>1);
+// console.log(2!=1);
+// console.log(2>=1);
+
+// console.log("2">1);
+// console.log("02">1);
+/*
+this all types of conversions are always confusing avoid always using these 
+// console.log(null > 0);
+// console.log(null>=0);
+// console.log(null==0);
+
+// console.log(undefined==0);
+// console.log(undefined>=0);
+// console.log(undefined>0);
+*/
+// === strict check it also checks for datatype 
+console.log("2" === 2);
